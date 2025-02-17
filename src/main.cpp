815fa88d4e5c5f992249aecc19f842c61e4c1289
hjc2000 <hjc.666@foxmail.com>
2025-02-17 11:25:01 +0800

    #include <base/time/time.h>

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,5 @@
+#include <base/time/time.h>
 #include <poco-wrapper/NTPClient.h>
-#include <time.h>
-
-void set_system_time(int64_t utc_time_microseconds)
-{
-	struct timespec ts;
-
-	// 将微秒转换为秒和纳秒
-	// 由于1秒 = 1,000,000个微秒单位
-	ts.tv_sec = utc_time_microseconds / 1000000;           // 整秒部分
-	ts.tv_nsec = (utc_time_microseconds % 1000000) * 1000; // 纳秒部分
-
-	// 设置系统时间
-	int result = clock_settime(CLOCK_REALTIME, &ts);
-	if (result != 0)
-	{
-		throw std::runtime_error("设置系统时间失败，请检查是否是管理员权限。");
-	}
-}
 
 int main()
 {
@@ -34,6 +17,7 @@ int main()
 	 * 操作系统自带时区功能，所以设置时间时不要手动处理时区带来的时间偏移，
 	 * 否则会造成偏移 2 次，变成 UTC+16.
 	 */
-	set_system_time(accurate_time.epochMicroseconds());
+	base::TimePointSinceEpoch time_point{std::chrono::microseconds{accurate_time.epochMicroseconds()}};
+	base::time::SetRealTime(time_point);
 	return 0;
 }
